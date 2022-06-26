51a3f3e4026b5a0c63ddc7f924c59b666b45ab24
jorge <jmaciasvesga@gmail.com>
2022-06-26 12:24:35 -0300

    teste

--- requisito_03.js
+++ requisito_03.js
@@ -17,7 +17,15 @@ define(["N/search"], function (search) {
         "AND",
         ["trandate", "within", "lastmonth"],
       ],
-      columns: ["total", "memo", "refnumber", "location"],
+      columns: [
+        "total", 
+        "memo", 
+        "refnumber", 
+        "location", 
+        search.createColumn({
+        name: "internalid",
+        join: "vendor"
+     })],
     });
 
     return vendorbillSearh;
@@ -28,12 +36,16 @@ define(["N/search"], function (search) {
    */
   function map(scriptContext) {
     try {
+      var vendor = Number(runtime.getCurrentScript().getParameter({ name: '	custscript_param_vendor' }));
+      log.debug('vendor del map', vendor)
       log.debug("map...", scriptContext.value);
       var searchResult = JSON.parse(scriptContext.value);
       var total = searchResult.values.total;
       var memo = searchResult.values.memo;
       var refNumber = searchResult.values.refNumber;
-      var location = searchResult.values.location;
+      var location = searchResult.values.location.value;
+      var vendorS = searchResult.values.location.value;
+      log.debug("vendorS", vendorS);
       log.debug("total", total);
       log.debug("location", location);
     } catch (error) {
